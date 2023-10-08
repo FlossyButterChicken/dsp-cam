99c2b5b8a0085752ea4250a35bdc93404e1ddba4
Avelino-Basco <126433309+Avelino-Basco@users.noreply.github.com>
2023-10-08 17:31:01 +0800

    Add files via upload
    added virtual camera via obs feature

--- Real-Time-Video-Stitching-using-OpenCV-in-Python-3.7-master/realtime_stitching.py
+++ Real-Time-Video-Stitching-using-OpenCV-in-Python-3.7-master/realtime_stitching.py
@@ -11,6 +11,8 @@ import imutils
 import time
 import cv2
 
+import pyvirtualcam
+
 # initialize the video streams and allow them to warmup
 #print("[INFO] starting cameras...")
 # rightStream = VideoStream(src="http://localhost/moto.mp4").start()  #aaka
@@ -28,72 +30,47 @@ leftStream = cv2.VideoCapture(1)
 stitcher = Stitcher()
 total = 0
 
-# loop over frames from the video streams
-while True:
-	# grab the frames from their respective video streams
-	ret, left = leftStream.read()
-	ret1, right = rightStream.read()
+with pyvirtualcam.Camera(800, 300, fps=30) as cam:
+	print(f'Virtual cam started: {cam.device} ({cam.width}x{cam.height} @ {cam.fps}fps)')
 
-	# resize the frames
-	left = imutils.resize(left, width=400)
-	right = imutils.resize(right, width=400)
 
-	# stitch the frames together to form the panorama
-	# IMPORTANT: you might have to change this line of code
-	# depending on how your cameras are oriented; frames
-	# should be supplied in left-to-right order
-	result = stitcher.stitch([left, right])
+	# loop over frames from the video streams
+	while True:
+		# grab the frames from their respective video streams
+		ret, left = leftStream.read()
+		ret1, right = rightStream.read()
 
-	# no homograpy could be computed
-	if result is None:
-		print("[INFO] homography could not be computed")
-		break
+		# resize the frames
+		left = imutils.resize(left, width=400)
+		right = imutils.resize(right, width=400)
 
-	# convert the panorama to grayscale, blur it slightly, update
-	# the motion detector
-	#gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-	#gray = cv2.GaussianBlur(gray, (21, 21), 0)
-	#locs = motion.update(gray)
+		# stitch the frames together to form the panorama
+		# IMPORTANT: you might have to change this line of code
+		# depending on how your cameras are oriented; frames
+		# should be supplied in left-to-right order
+		result = stitcher.stitch([left, right])
 
-	# only process the panorama for motion if a nice average has
-	# been built up
-	#if total > 32 and len(locs) > 0:
-		# initialize the minimum and maximum (x, y)-coordinates,
-		# respectively
-	#	(minX, minY) = (np.inf, np.inf)
-	#	(maxX, maxY) = (-np.inf, -np.inf)
+		result_rgb = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
 
-		# loop over the locations of motion and accumulate the
-		# minimum and maximum locations of the bounding boxes
-		# for l in locs:
-		# 	(x, y, w, h) = cv2.boundingRect(l)
-		# 	(minX, maxX) = (min(minX, x), max(maxX, x + w))
-		# 	(minY, maxY) = (min(minY, y), max(maxY, y + h))
+		# no homograpy could be computed
+		if result is None:
+			print("[INFO] homography could not be computed")
+			break
 
-		# # draw the bounding box
-		# cv2.rectangle(result, (minX, minY), (maxX, maxY),
-		# 	(0, 0, 255), 3)
+		# show the output images
+		cv2.imshow("Result", result)
+		cv2.imshow("Left Frame", left)
+		cv2.imshow("Right Frame", right)
+		key = cv2.waitKey(1) & 0xFF
 
-	# increment the total number of frames read and draw the 
-	# timestamp on the image
-	# total += 1
-	# timestamp = datetime.datetime.now()
-	# ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-	# cv2.putText(result, ts, (10, result.shape[1] - 10),
-	# cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+		cam.send(cv2.flip(result_rgb, 1))
+		cam.sleep_until_next_frame()
 
-	# show the output images
-	cv2.imshow("Result", result)
-	cv2.imshow("Left Frame", left)
-	cv2.imshow("Right Frame", right)
-	key = cv2.waitKey(1) & 0xFF
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
 
 # do a bit of cleanup
-print("[INFO] cleaning up...")
 cv2.destroyAllWindows()
 leftStream.release()
 rightStream.release()
